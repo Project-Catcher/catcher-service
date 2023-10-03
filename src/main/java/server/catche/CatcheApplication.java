6b83930c98c7ac8ff5f090ded5ba40969913380a
inyoung <charmingyear@gmail.com>
2023-10-04 04:19:45 +0900

    프로젝트 생성

---
+++ src/main/java/server/catche/CatcheApplication.java
@@ -0,0 +1,13 @@
+package server.catche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatcheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatcheApplication.class, args);
+    }
+
+}
