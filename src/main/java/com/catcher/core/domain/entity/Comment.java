61310ba6dc6fc6af8f716302cf1bfad3e1ea4053
권덕주 <djgwon@lifegoeson.kr>
2023-10-03 21:04:54 +0900

    feat: 댓글 관련 entity, repository 관련 코드 추가

---
+++ src/main/java/com/catcher/core/domain/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.catcher.core.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> replies = new ArrayList<>();
+
+    private String contents;
+}
