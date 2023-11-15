69c5fe9d2234f5547dd4479c776535eedf3cf91e
권덕주 <djgwon@lifegoeson.kr>
2023-11-16 01:17:56 +0900

    feat: entity 추가

---
+++ src/main/java/com/catcher/core/domain/entity/Category.java
@@ -0,0 +1,26 @@
+package com.catcher.core.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public static Category create(String name) {
+        Category category = new Category();
+        category.name = name;
+        return category;
+    }
+}
+
