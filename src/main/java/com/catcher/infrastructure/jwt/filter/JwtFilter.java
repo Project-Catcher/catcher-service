839f48bb2c6e853889a15c66773b69bfa8dbd58c
HongGeun <ghdrms163@gmail.com>
2023-11-27 23:15:49 +0900

    jwt 기능 마이그레이션

---
+++ src/main/java/com/catcher/infrastructure/jwt/filter/JwtFilter.java
@@ -0,0 +1,42 @@
+package com.catcher.infrastructure.jwt.filter;
+
+import com.catcher.infrastructure.jwt.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = getAccessToken(request);
+
+        if (accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(HttpServletRequest request) {
+        String header = request.getHeader(AUTHORIZATION);
+
+        if (header != null && header.startsWith("Bearer ")) {
+            header = header.substring(7);
+        }
+        return header;
+    }
+}
---
+++ src/main/java/com/catcher/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.catcher.security;
+
+import com.catcher.core.db.UserRepository;
+import com.catcher.core.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow();
+        return new CatcherUser(user);
+    }
+}
