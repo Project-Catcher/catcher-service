e8ded24a12bc1b42eeeba389380a70bc3848e7a4
inyoung <charmingyear@gmail.com>
2023-12-19 06:28:12 +0900

    refactor: dto null 관련 검사 추가

--- src/main/java/com/catcher/core/dto/request/SaveScheduleDetailRequest.java
+++ src/main/java/com/catcher/core/dto/request/SaveScheduleDetailRequest.java
@@ -2,6 +2,8 @@ package com.catcher.core.dto.request;
 
 import com.catcher.common.utils.customValid.valid.ValidEnum;
 import com.catcher.core.domain.entity.enums.ItemType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -18,9 +20,12 @@ public class SaveScheduleDetailRequest {
 
     private String description;
 
+    @NotBlank(message = "색상을 선택해주세요.")
     private String color;
 
+    @NotNull(message = "시작 시간을 지정해주세요.")
     private LocalDateTime startAt;
 
+    @NotNull(message = "종료 시간을 지정해주세요.")
     private LocalDateTime endAt;
 }
