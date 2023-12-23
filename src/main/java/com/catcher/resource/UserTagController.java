0ea16f0d3860bc99210949e873e1e28fd66c5bef
HongGeun <ghdrms163@gmail.com>
2023-12-24 00:32:36 +0900

    add auth annotation

--- src/main/java/com/catcher/resource/UserTagController.java
+++ src/main/java/com/catcher/resource/UserTagController.java
@@ -2,9 +2,11 @@ package com.catcher.resource;
 
 import com.catcher.common.response.CommonResponse;
 import com.catcher.core.domain.entity.User;
+import com.catcher.core.domain.entity.enums.UserRole;
 import com.catcher.core.dto.request.UserTagRequest;
 import com.catcher.core.dto.response.UserTagResponse;
 import com.catcher.core.service.UserTagService;
+import com.catcher.security.annotation.AuthorizationRequired;
 import com.catcher.security.annotation.CurrentUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ public class UserTagController {
     private final UserTagService userTagService;
 
     @PutMapping
+    @AuthorizationRequired(UserRole.USER)
     public CommonResponse<Void> updateUserTags(
             @RequestBody UserTagRequest userTagRequest,
             @CurrentUser User user
@@ -26,6 +29,7 @@ public class UserTagController {
     }
 
     @GetMapping
+    @AuthorizationRequired(UserRole.USER)
     public CommonResponse<UserTagResponse> findUserTags(@CurrentUser User user) {
         return CommonResponse.success(userTagService.findTagsByUser(user));
     }
