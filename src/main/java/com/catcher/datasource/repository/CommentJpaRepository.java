29b2f80ab97fbae7e5ef87182fc991f63f58629d
toad <toad0403@gmail.com>
2023-12-24 12:32:56 +0900

    delete unused code

--- src/main/java/com/catcher/datasource/repository/CommentJpaRepository.java
+++ src/main/java/com/catcher/datasource/repository/CommentJpaRepository.java
@@ -1,22 +1,8 @@
 package com.catcher.datasource.repository;
 
 import com.catcher.core.domain.entity.Comment;
-import com.catcher.core.dto.CommentWithLikesDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
-    @Query("SELECT new com.catcher.core.dto.CommentWithLikesDto(" +
-            "c, " +
-            "(SELECT COUNT(*) FROM Like l WHERE l.type = 'COMMENT' AND l.targetId = c.id), " +
-            "CASE WHEN l.id IS NOT NULL THEN TRUE ELSE FALSE END)" +
-            "FROM Comment c " +
-            "LEFT JOIN Like l ON l.type = 'COMMENT' AND l.user.id = c.user.id AND l.targetId = c.id " +
-            "WHERE c.schedule.id = :scheduleId")
-    List<CommentWithLikesDto> findAllWithLikeCountAndLikeStatusByScheduleId(
-            @Param("scheduleId") Long scheduleId
-    );
+
 }
--- src/main/java/com/catcher/datasource/repository/CommentReplyJpaRepository.java
+++ src/main/java/com/catcher/datasource/repository/CommentReplyJpaRepository.java
@@ -4,18 +4,5 @@ import com.catcher.core.domain.entity.CommentReply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentReplyJpaRepository extends JpaRepository<CommentReply, Long> {
-/*
-    @Query("SELECT new com.catcher.core.dto.CommentReplyWithLikesDto(" +
-            "cr, " +
-            "(SELECT COUNT(*) FROM Like l WHERE l.type = 'COMMENTREPLY' AND l.targetId = cr.id), " +
-            "CASE WHEN l.id IS NOT NULL THEN TRUE ELSE FALSE END)" +
-            "FROM CommentReply cr " +
-            "LEFT JOIN Like l ON l.type = 'COMMENTREPLY' AND l.user.id = cr.user.id AND l.targetId = cr.id  " +
-            "WHERE cr.comment.schedule.id = :schedulefId")
-    List<CommentReplyWithLikesDto> findAllWithLikeCountAndLikeStatusByScheduleId(
-            @Param("scheduleId") Long scheduleId
-    );
-
- */
 
 }
