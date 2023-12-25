6af7066fdc733fc8d80d026a3ec427a00e5f10b9
toad <toad0403@gmail.com>
2023-12-25 15:56:10 +0900

    BaseTimeEntity 에서 createdAt, updatedAt 을 LocalDateTime으로 변경

--- src/main/java/com/catcher/core/domain/entity/BaseTimeEntity.java
+++ src/main/java/com/catcher/core/domain/entity/BaseTimeEntity.java
@@ -4,29 +4,21 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseTimeEntity {
-    protected static ZoneId ZONE = ZoneId.of("Asia/Seoul");
 
     @Column(name = "created_at")
-    private ZonedDateTime createdAt;
+    private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
-    private ZonedDateTime updatedAt;
-
-    @PrePersist
-    private void prePersist() {
-        this.createdAt = ZonedDateTime.now(ZONE);
-        this.updatedAt = ZonedDateTime.now(ZONE);
-    }
+    private LocalDateTime updatedAt;
 
     @PreUpdate
     private void preUpdate() {
-        this.updatedAt = ZonedDateTime.now(ZONE);
+        this.updatedAt = LocalDateTime.now();
     }
 }
