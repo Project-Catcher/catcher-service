0340adf834aaf2a0d409d0ddde20ae3eca6bd9d8
권덕주 <djgwon@lifegoeson.kr>
2023-12-26 21:10:00 +0900

    fix: 같은 상태로 변경하는 경우 처리하기 위하여 테이블 변경

--- src/main/java/com/catcher/core/domain/entity/UserStatusChangeHistory.java
+++ src/main/java/com/catcher/core/domain/entity/UserStatusChangeHistory.java
@@ -14,28 +14,30 @@ public class UserStatusChangeHistory extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    public User user;
+    private User user;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
-    public UserStatusChangeHistory parent;
+    private UserStatusChangeHistory parent;
 
     @OneToOne(mappedBy = "parent", fetch = FetchType.LAZY)
-    public UserStatusChangeHistory child;
+    private UserStatusChangeHistory child;
 
-    public UserStatus beforeStatus;
+    private UserStatus beforeStatus;
 
-    public UserStatus afterStatus;
+    private UserStatus afterStatus;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "changed_user_id", nullable = false)
-    public User changedUser;
+    private User changedUser;
 
-    public String reason;
+    private String reason;
+
+    private int currentStateCount = 0;
 
     public static UserStatusChangeHistory create(User user, User changedUser, UserStatusChangeHistory parent, UserStatus beforeStatus, UserStatus afterStatus, String reason) {
         return UserStatusChangeHistory.builder()
